feaa417b2deb3c2bce85937edd5122e08940cb6f
Yukon W <ywainczak@gmail.com>
2018-01-27 12:45:23 -0500

    Enemies
     + Slimey boi

--- Assets/Scripts/Scripty Bois/Enemy.cs
+++ Assets/Scripts/Scripty Bois/Enemy.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : ScriptableObject
 {
     public Color color;
     public Sprite sprite;
---
+++ Assets/Scripts/Scripty Bois/Tools/MakeEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif 
+
+public class MakeEnemy
+{
+	#if UNITY_EDITOR
+	[MenuItem("Assets/Create/Enemy")]
+	#endif
+	public static void Create()
+	{
+		Enemy asset = ScriptableObject.CreateInstance<Enemy>();
+		#if UNITY_EDITOR
+		AssetDatabase.CreateAsset(asset, "Assets/Enemies/newEnemy.asset");
+		AssetDatabase.SaveAssets();
+		EditorUtility.FocusProjectWindow();
+		Selection.activeObject = asset;
+		#endif
+	}
+}
