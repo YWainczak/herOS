0768076af0adcc8e952a6d2aabbbc9ed640862e1
Yukon W <ywainczak@gmail.com>
2018-01-26 21:49:58 -0500

    Fucking what was it

---
+++ Assets/Scripts/AppController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppController : MonoBehaviour
+{
+    public bool appOpen;
+    public Color appColor;
+    public Image appOverlayHeader;
+    public Text appOverlayHeaderText;
+
+    private RectTransform appRect;
+    private float appSpeed = 16;
+
+	// Use this for initialization
+	void Start ()
+    {
+        appRect = gameObject.GetComponent<RectTransform>();
+        appOverlayHeader.color = appColor;
+        appOverlayHeaderText.text = gameObject.name;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (appRect != null)
+        {
+            if (appOpen)
+            {
+                appRect.anchoredPosition = new Vector2(0, Mathf.Lerp(appRect.anchoredPosition.y, 0, appSpeed * Time.deltaTime));
+            }
+            else
+            {
+                appRect.anchoredPosition = new Vector2(0, Mathf.Lerp(appRect.anchoredPosition.y, -Screen.height, appSpeed * Time.deltaTime));
+            }
+        }
+	}
+
+    public void CloseApp()
+    {
+        appOpen = false;
+    }
+
+    public void OpenApp()
+    {
+        appOpen = true;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float health;
+    public float healthMax;
+
+    public List<AppController> apps;
+
+    public Image battery; 
+
+    void Start()
+    {
+        health = healthMax;
+    }
+
+    void Update()
+    {
+        battery.fillAmount = health / healthMax;
+    }
+
+    public void Home()
+    {
+        foreach(AppController controller in apps)
+        {
+            controller.CloseApp();
+        }
+    }
+
+    public void LaunchApp(string appName)
+    {
+        foreach (AppController controller in apps)
+        {
+            if (controller.gameObject.name == appName)
+            {
+                controller.OpenApp();
+            }
+        }
+    }
+}
