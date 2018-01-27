94d4657fc7e8859f4d8dac529983d83048b066b4
Tyler Haddad <ocarinasurge@gmail.com>
2018-01-27 12:28:33 -0500

    Player & Enemy Stats
    - Player script (includes player stats & variables)
    - Enemy script (includes enemy stats & variables)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Color color;
+    public Sprite sprite;
+    
+    public float health;
+    public float healthMax;
+
+    public float attack; // Attack stat
+    public float attackTime; // Time from telegraph until actual attack
+    public float attackTimeMax; // Max time for the attack timer
+    public float defense; // Defense stat
+    public float defenseTime; // Time for defense to be held
+    public float defenseTimeMax; //Max time for defense to be held
+}
